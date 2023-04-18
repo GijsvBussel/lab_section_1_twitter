a4992527ac443ee575caef9ee178443a7ab5cdf8
GijsvBussel <89523454+GijsvBussel@users.noreply.github.com>
2023-04-18 12:10:45 +0200

    Add files via upload

---
+++ students data collection twitter.R
@@ -0,0 +1,19 @@
+# collect_twitter_data.R
+#
+# Contributors:
+#
+# What this file does:
+#   * Uses rtweet to harvest twitter data
+#   * Shows various options available to restrict collection
+
+# --- Libraries --- #
+library(rtweet)  # Twitter API
+library(readr)   # read/write files
+
+#--- Basic API Usage ---#
+
+# --- Filtering the Search --- #
+
+# --- For network Visualization --- #
+
+# --- Saving the data --- #
---
+++ students plot data twitter.R
@@ -0,0 +1,20 @@
+# plot_twitter_network.R
+#
+# Contributors:
+#
+# What this file does:
+#   * Transforms returned Twitter data into a network of connections
+#   * Plots retweet and Mentions networks
+
+# --- Libraries --- #
+library(readr)     # read/write files
+library(dplyr)     # data manip
+library(tidyr)     # data manip
+library(tidygraph) # network data
+library(ggraph)    # network viz
+
+# --- Load Data --- #
+
+# --- Retweet Network --- #
+
+# --- Mentions Network --- #
